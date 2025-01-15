b2d7a8f21b0680238aa1d9db748650e763305512
Ayomide <ayomideayomipo147@gmail.com>
2025-01-15 21:39:58 +0100

    Post Routes Updated

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -14,4 +14,13 @@ router.post('/', createPost)
 // Get all posts
 router.get('/', getAllPosts)
 
+// Get a single post by ID
+router.get('/:id', getPostById)
+
+// Update a post by ID
+router.put('/:id', updatePost)
+
+// Delete a post by ID
+router.delete('/:id', deletePost)
+
 export default router
